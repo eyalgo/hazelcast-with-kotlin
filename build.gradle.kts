a046e35c4e5579f2dd53b2de339d1d6de7184182
Eyal Golan <eyal.golan@signavio.com>
2024-01-24 02:25:53 +0100

    remove micronaut (gradle test pass

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,6 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
     id("org.jlleitschuh.gradle.ktlint-idea") version "11.5.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("io.micronaut.application") version "4.2.1"
-    id("io.micronaut.aot") version "4.2.1"
 }
 
 version = "0.1"
@@ -21,22 +19,12 @@ repositories {
 }
 
 dependencies {
-    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
-    implementation("io.micronaut.serde:micronaut-serde-jackson")
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
 
     implementation("com.hazelcast:hazelcast:5.3.5")
-
-    compileOnly("io.micronaut:micronaut-http-client")
-    runtimeOnly("ch.qos.logback:logback-classic")
-    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
-    testImplementation("io.micronaut:micronaut-http-client")
 }
 
-application {
-    mainClass.set("eyalgo.ApplicationKt")
-}
 java {
     sourceCompatibility = JavaVersion.toVersion("17")
 }
@@ -54,26 +42,5 @@ tasks {
             "ktlintMainSourceSetCheck",
             "ktlintTestSourceSetCheck"
         )
-    }
-}
-
-graalvmNative.toolchainDetection.set(false)
-micronaut {
-    runtime("netty")
-    testRuntime("kotest5")
-    processing {
-        incremental(true)
-        annotations("eyalgo.*")
-    }
-    aot {
-        // Please review carefully the optimizations enabled below
-        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
-        optimizeServiceLoading.set(false)
-        convertYamlToJava.set(false)
-        precomputeOperations.set(true)
-        cacheEnvironment.set(true)
-        optimizeClassLoading.set(true)
-        deduceEnvironment.set(true)
-        optimizeNetty.set(true)
     }
 }
