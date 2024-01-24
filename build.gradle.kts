f5e4617a8f9d656a1f0be7f689e1c2200408a59a
Eyal Golan <eyal.golan@signavio.com>
2024-01-24 02:10:44 +0100

    Fix gradle (use micronaut as example) 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,84 +1,50 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    val kotlinVersion = "1.9.20"
+    // val kotlinVersion="1.9.21"
 
     java
-    jacoco
-    kotlin("jvm").version(kotlinVersion)
+    kotlin("jvm").version("1.9.21")
+
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.21"
     id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
     id("org.jlleitschuh.gradle.ktlint-idea") version "11.5.0"
+    id("com.google.devtools.ksp") version "1.9.21-1.0.16"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.micronaut.application") version "4.2.1"
+    id("io.micronaut.aot") version "4.2.1"
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
+version = "0.1"
 group = "eyalgo"
 
+val kotlinVersion = project.properties.get("kotlinVersion")
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    ksp("io.micronaut:micronaut-http-validation")
+    ksp("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
 
     implementation("com.hazelcast:hazelcast:5.3.5")
 
-    val jacksonVersion = "2.15.3"
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    implementation("com.fasterxml.jackson:jackson-bom:$jacksonVersion")
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-
-    implementation("io.arrow-kt:arrow-core:1.2.1")
-
-    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
-    testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.4.0")
-    testImplementation("org.awaitility:awaitility:4.2.0")
+    compileOnly("io.micronaut:micronaut-http-client")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testImplementation("io.micronaut:micronaut-http-client")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-
-    group = "verification"
-    testClassesDirs = sourceSets["test"].output.classesDirs
-    classpath = sourceSets["test"].runtimeClasspath
-
-    testLogging {
-        events = mutableSetOf(FAILED, PASSED, SKIPPED)
-        showExceptions = true
-        exceptionFormat = FULL
-        showCauses = true
-        showStackTraces = true
-        showStandardStreams = true
-
-        info.events = lifecycle.events
-        info.exceptionFormat = lifecycle.exceptionFormat
-
-        addTestListener(EyalgoGradleLoggingTestListener())
-    }
+application {
+    mainClass.set("eyalgo.ApplicationKt")
 }
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
-    }
-}
-
-sourceSets {
-    getByName("main").java.srcDirs("src/main/kotlin")
-    getByName("test").java.srcDirs("src/test")
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
 }
 
 tasks {
-
     addKtlintFormatGitPreCommitHook {
         mustRunAfter(
             "processResources",
@@ -92,69 +58,25 @@ tasks {
             "ktlintTestSourceSetCheck"
         )
     }
-
-    compileKotlin {
-        dependsOn(addKtlintFormatGitPreCommitHook)
-    }
-
-    test {
-        filter {
-            includeTestsMatching("*Test")
-            includeTestsMatching("*Test*.*")
-            excludeTestsMatching("*Architecture")
-        }
-    }
 }
 
-class EyalgoGradleLoggingTestListener : TestListener {
-    private val successTests = mutableListOf<TestDescriptor>()
-    private val failedTests = mutableListOf<TestDescriptor>()
-    private val skippedTests = mutableListOf<TestDescriptor>()
-
-    override fun beforeSuite(suite: TestDescriptor) {}
-    override fun beforeTest(testDescriptor: TestDescriptor) {}
-    override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
-        when (result.resultType) {
-            TestResult.ResultType.SUCCESS -> successTests.add(testDescriptor)
-            TestResult.ResultType.FAILURE -> failedTests.add(testDescriptor)
-            TestResult.ResultType.SKIPPED -> skippedTests.add(testDescriptor)
-            else -> Unit
-        }
+graalvmNative.toolchainDetection.set(false)
+micronaut {
+    runtime("netty")
+    testRuntime("kotest5")
+    processing {
+        incremental(true)
+        annotations("eyalgo.*")
     }
-
-    override fun afterSuite(suite: TestDescriptor, result: TestResult) {
-        if (suite.parent == null) { // root suite
-            printTestResults(successTests, failedTests, skippedTests, result)
-        }
+    aot {
+        // Please review carefully the optimizations enabled below
+        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading.set(false)
+        convertYamlToJava.set(false)
+        precomputeOperations.set(true)
+        cacheEnvironment.set(true)
+        optimizeClassLoading.set(true)
+        deduceEnvironment.set(true)
+        optimizeNetty.set(true)
     }
-
-    private fun printTestResults(
-        successTests: List<TestDescriptor>,
-        failedTests: List<TestDescriptor>,
-        skippedTests: List<TestDescriptor>,
-        result: TestResult
-    ) {
-        println("---- Test results: ${result.resultType} ----")
-        println(
-            "Test summary: ${result.testCount} tests, " +
-                "${result.successfulTestCount} succeeded, " +
-                "${result.failedTestCount} failed, " +
-                "${result.skippedTestCount} skipped" +
-                "in ${(result.endTime - result.startTime).toFloat() / 1000}s"
-        )
-
-        println()
-        skippedTests.prefixedSummary("--- Skipped Tests: ---")
-        println()
-        successTests.prefixedSummary("--- Success Tests: ---")
-        println()
-        failedTests.prefixedSummary("--- Failed Tests: ---")
-    }
-
-    private fun List<TestDescriptor>.prefixedSummary(subject: String) {
-        print(subject + System.lineSeparator())
-        forEach { test -> println("${test.className} [${test.displayName}]") }
-    }
-
-    private fun TestDescriptor.displayName() = parent?.let { "${it.name} - $name" } ?: name
 }
